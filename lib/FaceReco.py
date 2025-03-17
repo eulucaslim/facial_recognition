0d130c114919a0523ca5b5f11ca8c815f4c44605
eulucaslim <lucaslimacnt14@gmail.com>
2025-03-16 22:28:55 -0400

    Remove the Deepface

--- lib/FaceReco.py
+++ lib/FaceReco.py
@@ -1,11 +1,7 @@
-import os
-os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
-import tensorflow as tf
-from deepface import DeepFace
 from datetime import datetime
 import cv2
 import shutil
-
+import os
 
 class FaceReco:
 
@@ -42,13 +38,10 @@ class FaceReco:
             with open(temp_file_path, "wb+") as temp_file:
                 shutil.copyfileobj(img_bytes, temp_file)
             
-            for files in os.listdir(self.users_folder):
-                result = DeepFace.verify(
-                    img1_path=temp_file_path, 
-                    img2_path=os.path.join(self.users_folder, files)
-                )
-                if result['verified']:
-                    print("Found The Image in Database")
+            result = {}
+
+            if result['verified']:
+                print("Found The Image in Database")
             
             if result['verified'] == False:
                 print("User don't have access!")
